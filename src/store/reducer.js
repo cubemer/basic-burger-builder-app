7d6b1296841110a894e0285c956603c0f3d321ed
Juan Carmona <xjuanchovillax@gmail.com>
2019-06-10 21:54:35 -0600

    Add basic redux functionality
    I set up a skeleton for redux functionality in the app.

---
+++ src/store/reducer.js
@@ -0,0 +1,10 @@
+import * as types from './actions';
+
+const initialState = {
+  ingredients: null,
+  totalPrice: 2,
+};
+
+const reducer = (state = initialState, action) => {}
+
+export default reducer;
