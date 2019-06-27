362706c2cc159d5ef4fb7031f1fa4c254f59f115
Juan Carmona <xjuanchovillax@gmail.com>
2019-06-26 21:00:08 -0600

    Add Authentication page to the app
    I added a basic auth page to the app, it currently has no more functionality other than validating the email address field and password field

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -114,19 +114,15 @@ class ContactData extends React.Component {
 
   checkValidity(value, rules) {
     let isValid = true;
-
     if (rules.required) {
       isValid = value.trim() !== '' && isValid;
     }
-
     if (rules.minLength) {
       isValid = value.length >= rules.minLength && isValid;
     }
-
     if (rules.maxLength) {
       isValid = value.length <= rules.maxLength && isValid;
     }
-
     return isValid
   }
 
