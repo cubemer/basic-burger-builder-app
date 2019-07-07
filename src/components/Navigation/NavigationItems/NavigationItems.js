5a066b63161759824be541b43c18ac49d404d24b
Juan Carmona <xjuanchovillax@gmail.com>
2019-07-06 21:57:44 -0600

    Add a logout link
    Previously the user wouldn't be able to tell from the UI if they were signed in now, if a user is signed in they will have the option to sign out.

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -3,11 +3,14 @@ import React from 'react';
 import styles from './NavigationItems.module.css';
 import NavigationItem from './NavigationItem/NavigationItem';
 
-const navigationItems = () => (
+const navigationItems = (props) => (
   <ul className={styles.NavigationItems}>
     <NavigationItem link='/' exact>Burger Builder</NavigationItem>
     <NavigationItem link='/orders'>Orders</NavigationItem>
-    <NavigationItem link='/auth'>Authenticate</NavigationItem>
+    { !props.isAuthenticated
+      ? <NavigationItem link='/auth'>Authenticate</NavigationItem>
+      : <NavigationItem link='/logout'>Logout</NavigationItem>
+    }
   </ul>
 );
 
--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
 
 import Aux from '../Aux/Aux';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
@@ -24,8 +25,11 @@ class Layout extends Component {
   render() {
     return (
       <Aux>
-        <Toolbar drawerToggleClicked={this.sideDrawerToggleHandler}/>
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          drawerToggleClicked={this.sideDrawerToggleHandler}/>
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           isShown={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler} />
         <main className={styles.Content}>
@@ -36,4 +40,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = state => (
+  {
+    isAuthenticated: state.auth.token !== null
+  }
+)
+
+export default connect(mapStateToProps)(Layout);
