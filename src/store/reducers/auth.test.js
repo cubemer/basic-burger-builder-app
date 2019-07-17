9f250ab6c221e07c6dd527ff2307072991c78332
Juan Carmona <xjuanchovillax@gmail.com>
2019-07-16 22:00:04 -0600

    Add reducer tests

---
+++ src/store/reducers/auth.test.js
@@ -0,0 +1,34 @@
+import reducer from './auth';
+import * as actions from '../actions/actionTypes';
+
+describe('auth reducer', () => {
+  it('should return the inital state', () => {
+    expect(reducer(undefined, {})).toEqual({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    });
+  })
+
+  it('should store the token upon login', () => {
+    expect(reducer({
+      token: null,
+      userId: null,
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    }, {
+      action: actions.AUTH_SUCESS,
+      idToken: 'token',
+      userId: 'user-id'
+    })).toEqual({
+      token: 'token',
+      userId: 'user-id',
+      error: null,
+      loading: false,
+      authRedirectPath: '/'
+    })
+  })
+})
